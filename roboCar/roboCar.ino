d187846b87a747c7482d6ba9d626647133240718
Paweł Spychalski <pawel@spychalski.info>
2013-04-13 18:10:40 +0200

    First, basic version or robot behavior

--- roboCar/roboCar.ino
+++ roboCar/roboCar.ino
@@ -7,79 +7,140 @@ int right_state = 2;
 
 int action_delay = 2000;
 
-int currentSpeed = 255;
+int currentSpeed = 210;
+
+int rangeTrig = 8;
+int rangeEcho = 7;
+
+int stopRange = 25;
+
+char currentAction = ' ';
 
 void setup() {
   
-  pinMode(led, OUTPUT);
+  // Engines
   pinMode(left_pwm, OUTPUT);
   pinMode(left_state, OUTPUT);
   pinMode(right_pwm, OUTPUT);
   pinMode(right_state, OUTPUT);
   
+  // Engines switch
+  pinMode(engines_switch, INPUT_PULLUP);
+  
+  //Range meter
+  pinMode(rangeTrig, OUTPUT);
+  pinMode(rangeEcho, INPUT);
+    
 }
 
+long getRange() {
+  
+  long duration, distance;
+  digitalWrite(rangeTrig, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(rangeTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(rangeTrig, LOW);
+  duration = pulseIn(rangeEcho, HIGH);
+  distance = (duration/2) / 29.1;
+  
+  return distance;
+}
+
+
 void forward() {
-  analogWrite(left_pwm,currentSpeed);
-  digitalWrite(left_state,0);
-  analogWrite(right_pwm,currentSpeed);
-  digitalWrite(right_state,0);
+  
+  currentAction = 'f';
+  
+  if (digitalRead(engines_switch) == LOW) {
+  
+    analogWrite(left_pwm,currentSpeed);
+    digitalWrite(left_state,0);
+    analogWrite(right_pwm,currentSpeed);
+    digitalWrite(right_state,0);
+  
+  }else {
+    engine_stop();
+  }
+  
 }
 
 void back() {
-  analogWrite(left_pwm,255-currentSpeed);
-  digitalWrite(left_state,1);
-  analogWrite(right_pwm,255-currentSpeed);
-  digitalWrite(right_state,1);
+  
+  currentAction = 'b';
+  
+  if (digitalRead(engines_switch) == LOW) {
+  
+    analogWrite(left_pwm,255-currentSpeed);
+    digitalWrite(left_state,1);
+    analogWrite(right_pwm,255-currentSpeed);
+    digitalWrite(right_state,1);
+  
+  }else {
+    engine_stop();
+  }
 }
 
 void engine_stop() {
+  
+  currentAction = ' ';
+  
   analogWrite(left_pwm,0);
   digitalWrite(left_state,0);
   analogWrite(right_pwm,0);
   digitalWrite(right_state,0);
 }
 
-void turn_l-eft() {
-  analogWrite(left_pwm,currentSpeed);
-  digitalWrite(left_state,0);
-  analogWrite(right_pwm,255-currentSpeed);
-  digitalWrite(right_state,1);
+void turn_left() {
+  
+  currentAction = 'l';
+  
+  if (digitalRead(engines_switch) == LOW) {
+  
+    analogWrite(left_pwm,currentSpeed);
+    digitalWrite(left_state,0);
+    analogWrite(right_pwm,255-currentSpeed);
+    digitalWrite(right_state,1);
+  
+  }
 }
 
 void turn_right() {
-  analogWrite(left_pwm,255-currentSpeed);
-  digitalWrite(left_state,1);
-  analogWrite(right_pwm,currentSpeed);
-  digitalWrite(right_state,0);
+  
+  currentAction = 'r';
+  
+  if (digitalRead(engines_switch) == LOW) {
+  
+    analogWrite(left_pwm,255-currentSpeed);
+    digitalWrite(left_state,1);
+    analogWrite(right_pwm,currentSpeed);
+    digitalWrite(right_state,0);
+  
+  }
+  
 }
 
-/*
+long currentRange;
+int actionDelay = 50;
+
 void loop() {
   
+  currentRange = getRange();
+  
+  if (currentRange > stopRange) {
+    forward(); 
+    actionDelay = 50;
+  }else {
+    
+    if (currentAction != 'l') {
+      engine_stop();
+      delay(500);
+    }
+    
+    turn_left();
+    actionDelay = 800;
+  }
+  
+  delay(actionDelay);
   
 }
-*/
-
-Demo version
-void loop() {
-  
-  forward();
-  delay(2000);
-  engine_stop();
-  delay(500);
-  back();
-  delay(2000);
-  engine_stop();
-  delay(500);
-  turn_left();
-  delay(1000);
-  engine_stop();
-  delay(500);
-  turn_right();
-  delay(1000);
-  engine_stop();
-  delay(500);
-  
-}
-
